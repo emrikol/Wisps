db5afa6f8e623f2b3fb8f10c9481bf119fe1a9fa
Derrick Tennant <derrick.tennant@automattic.com>
2020-02-14 13:34:15 -0500

    Cleaned up Javacript

--- inc/class-wisp-cpt.php
+++ inc/class-wisp-cpt.php
@@ -314,150 +314,24 @@ class Wisps_CPT {
 
 		// Mostly borrowed from core's `get_post_embed_html()`.
 		$embed_url = get_post_embed_url( $post );
+		$wisp_data = base64_decode( get_post_meta( $post->ID, '_wisp_data', true ) );
 
-		$output = '<blockquote class="wp-embedded-content"><a href="' . esc_url( get_permalink( $post ) ) . '">' . esc_html( get_the_title( $post ) ) . "</a></blockquote>\n";
-		$output .= "<script type='text/javascript'>\n";
-		$output .= "<!--//--><![CDATA[//><!--\n";
+		ob_start();
+?>
+<div class="wisp-embed">
+	<div id="gist-data">
+		<pre><code class="line-numbers <?php echo sanitize_html_class( $mime_type ); ?>"><?php echo esc_html( $wisp_data ); ?></code></pre>
+	</div>
 
-		$output .= <<< 'EOT'
-/**
- * WordPress inline HTML embed
- *
- * @since 4.4.0
- * @output wp-includes/js/wp-embed.js
- *
- * This file cannot have ampersands in it. This is to ensure
- * it can be embedded in older versions of WordPress.
- * See https://core.trac.wordpress.org/changeset/35708.
- */
-(function ( window, document ) {
-	'use strict';
-	console.log( 'here!' );
-
-	var supportedBrowser = false,
-		loaded = false;
-
-		if ( document.querySelector ) {
-			if ( window.addEventListener ) {
-				supportedBrowser = true;
-			}
-		}
-
-	/** @namespace wp */
-	window.wp = window.wp || {};
-
-	if ( !! window.wp.receiveEmbedMessage ) {
-		return;
-	}
-
-	window.wp.receiveEmbedMessage = function( e ) {
-		var data = e.data;
-
-		if ( ! data ) {
-			return;
-		}
-
-		if ( ! ( data.secret || data.message || data.value ) ) {
-			return;
-		}
-
-		if ( /[^a-zA-Z0-9]/.test( data.secret ) ) {
-			return;
-		}
-
-		console.log( 'have data' );
-
-		var iframes = document.querySelectorAll( 'iframe[data-secret="' + data.secret + '"]' ),
-			blockquotes = document.querySelectorAll( 'blockquote[data-secret="' + data.secret + '"]' ),
-			i, source, height, sourceURL, targetURL;
-
-		for ( i = 0; i < blockquotes.length; i++ ) {
-			blockquotes[ i ].style.display = 'none';
-		}
-
-		for ( i = 0; i < iframes.length; i++ ) {
-			source = iframes[ i ];
-
-			if ( e.source !== source.contentWindow ) {
-				continue;
-			}
-
-			source.removeAttribute( 'style' );
-
-			/* Resize the iframe on request. */
-			if ( 'height' === data.message ) {
-				height = parseInt( data.value, 10 );
-				if ( height > 1000 ) {
-					height = 1000;
-				} else if ( ~~height < 200 ) {
-					height = 200;
-				}
-
-				source.height = height;
-			}
-
-			/* Link to a specific URL on request. */
-			if ( 'link' === data.message ) {
-				sourceURL = document.createElement( 'a' );
-				targetURL = document.createElement( 'a' );
-
-				sourceURL.href = source.getAttribute( 'src' );
-				targetURL.href = data.value;
-
-				/* Only continue if link hostname matches iframe's hostname. */
-				if ( targetURL.host === sourceURL.host ) {
-					if ( document.activeElement === source ) {
-						window.top.location.href = data.value;
-					}
-				}
-			}
-		}
-	};
-
-	function onLoad() {
-		if ( loaded ) {
-			return;
-		}
-
-		loaded = true;
-
-		var isIE10 = -1 !== navigator.appVersion.indexOf( 'MSIE 10' ),
-			isIE11 = !!navigator.userAgent.match( /Trident.*rv:11\./ ),
-			iframes = document.querySelectorAll( 'iframe.wp-embedded-content' ),
-			iframeClone, i, source, secret;
-
-		for ( i = 0; i < iframes.length; i++ ) {
-			source = iframes[ i ];
-
-			if ( ! source.getAttribute( 'data-secret' ) ) {
-				/* Add secret to iframe */
-				secret = Math.random().toString( 36 ).substr( 2, 10 );
-				source.src += '#?secret=' + secret;
-				source.setAttribute( 'data-secret', secret );
-			}
-
-			/* Remove security attribute from iframes in IE10 and IE11. */
-			if ( ( isIE10 || isIE11 ) ) {
-				iframeClone = source.cloneNode( true );
-				iframeClone.removeAttribute( 'security' );
-				source.parentNode.replaceChild( iframeClone, source );
-			}
-		}
-	}
-
-	if ( supportedBrowser ) {
-		window.addEventListener( 'message', window.wp.receiveEmbedMessage, false );
-		document.addEventListener( 'DOMContentLoaded', onLoad, false );
-		window.addEventListener( 'load', onLoad, false );
-	}
-})( window, document );
-EOT;
-
-		$output .= "\n//--><!]]>";
-		$output .= "\n</script>";
-
-		$output .= sprintf(
-			'<iframe sandbox="allow-scripts" security="restricted" src="%1$s" width="%2$d" height="%3$d" title="%4$s" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" class="wp-embedded-content"></iframe>',
+	<div class="wisp-meta">
+		<a class='view-raw' href="<?php echo esc_url( trailingslashit( get_permalink( $post->ID ) ) . 'raw/' ); ?>" style="float:right">view raw</a>
+		<a class='permalink' href="<?php echo esc_url( get_permalink( $post->ID ) ); ?>"><?php echo esc_html( get_the_title( $post->ID ) ); ?></a>
+		displayed with ❤ by <a href="https://github.com/emrikol/wisps">Wisps</a>
+	</div>
+</div>
+<script type='text/javascript'><?php echo file_get_contents( WPINC . '/js/wp-embed.js' ); ?></script>
+<?php printf(
+			'<iframe sandbox="allow-scripts" security="restricted" src="%1$s" width="%2$d" height="%3$d" title="%4$s" frameborder="0" marginwidth="0" marginheight="0" scrolling="yes" class="wp-embedded-content wisp-embedded-content" style="width: 100%;"></iframe>',
 			esc_url( $embed_url ),
 			absint( $width ),
 			absint( $height ),
@@ -471,8 +345,7 @@ EOT;
 			)
 		);
 
-		return $output;
+		return ob_get_clean();
 	}
-
 
 }
